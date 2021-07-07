b5c220f31d6f9122b829388c1485b1ca08dc22ed
enzoperce35 <enzoperce35@gmail.com>
2021-07-07 14:36:36 +0800

    finalize routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'users/new'
-  get 'users/create'
-  resources :users, only: [:new, :create]
+  
+  resources :users, only: [:update, :new, :create, :edit]
 end
